3a67d52ca1ce8f62045c783ba587d4a5955fa51e
satlawa <p.satlawa@gmx.net>
2020-12-28 09:06:48 +0100

    task1 finished

--- P0/Task1.py
+++ P0/Task1.py
@@ -14,7 +14,20 @@ with open('calls.csv', 'r') as f:
 
 """
 TASK 1:
-How many different telephone numbers are there in the records? 
+How many different telephone numbers are there in the records?
 Print a message:
 "There are <count> different telephone numbers in the records."
 """
+
+def get_tel_num(input_list):
+    """ extract all unique telephone numbers from provided lists """
+    tel_num = set()
+    for recs in input_list:
+        for rec in recs:
+            for i in range(2):
+                tel_num.add(rec[i])
+    return tel_num
+
+count_tel_num = len(get_tel_num([texts, calls]))
+
+print("There are {} different telephone numbers in the records.".format(count_tel_num))
